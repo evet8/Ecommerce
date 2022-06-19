2490b30833288f738fd46a1c9767aae1cc3277f0
evet8 <evetorrejos@gmail.com>
2022-06-20 07:13:50 +0800

    added countdown timer

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function getInfo() {
 	var password = document.getElementById('password').value
 
 	for(var i = 0; i < userCredentials.length; i++) {
-		// check if user input matches username and password of a current index of the objPeople array
+		// check if user input matches username and password of userCredentials array
 		if(username == userCredentials[i].username && password == userCredentials[i].password) {
 			alert(username + " is logged in!!!")
 			// stop the function if this is found to be true
@@ -27,4 +27,18 @@ function getInfo() {
 		}
 	}
 	alert("incorrect username or password")
+}
+
+
+const startingMinutes = 60;
+let time = startingMinutes * 60;
+const countdownEl = document.getElementById('countdown');
+setInterval(updateCountdown, 1000);
+function updateCountdown() {
+	const minutes = Math.floor(time / 60);
+	let seconds = time % 60;
+	seconds = seconds < 10 ? '0' + seconds : seconds;
+	countdownEl.innerHTML = minutes + ':' + seconds;
+	time--;
+	time = time < 0 ? 0 : time; 
 }
