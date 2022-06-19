4bf6ef85b1cde8054803f520b524b9f70cc13802
evet8 <evetorrejos@gmail.com>
2022-06-20 06:51:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var objPeople = [
+var userCredentials = [
 	{ 
 		username: "Ernest",
 		password: "Password1"
@@ -18,13 +18,13 @@ function getInfo() {
 	var username = document.getElementById('username').value
 	var password = document.getElementById('password').value
 
-	for(var i = 0; i < objPeople.length; i++) {
-		// check is user input matches username and password of a current index of the objPeople array
-		if(username == objPeople[i].username && password == objPeople[i].password) {
-			window.alert(username + " is logged in!!!")
+	for(var i = 0; i < userCredentials.length; i++) {
+		// check if user input matches username and password of a current index of the objPeople array
+		if(username == userCredentials[i].username && password == userCredentials[i].password) {
+			alert(username + " is logged in!!!")
 			// stop the function if this is found to be true
 			return
 		}
 	}
-	window.alert("incorrect username or password")
+	alert("incorrect username or password")
 }
